e1d5ae0231e6b3c1e2b42201083341c95dae6120
CrownWalker1 <cameron.robert.benson@gmail.com>
2025-04-01 18:41:27 -0700

    changes

--- app.vue
+++ app.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
-    <header class="text-3xl">Camerons Website</header>
+    <header class="text-3xl">Camerons Website 123</header>
     <NuxtPage />
+
+
   </div>
 
 </template>
