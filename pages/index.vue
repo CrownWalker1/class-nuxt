b6bd93d2649f8efab09c1e022c5dc0db4e248a27
CrownWalker1 <cameron.robert.benson@gmail.com>
2025-04-22 00:56:28 -0600

    Homework Complete

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,23 @@
 <template>
+    <div class="max-7xl mx-auto">
+
+        <section>
+            <img src="https://images.unsplash.com/photo-1743710426934-89887ca897d8?q=80&w=2138&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt=""
+
+                class="w-full h-96 object-cover"
+                />
+
+        </section>
+<section class="flex flex-row gap-8 py-12 justify-between">
+    <article class="flex-1 h-96 bg-gray-400 text-white text-center">Box 1</article>
+
+    <article class="flex-1 h-96 bg-gray-400 text-white text-center">Box 2</article>
     
+    <article class="flex-1 h-96 bg-gray-400 text-white text-center">Box 3</article>
+
+</section>
+
+    </div>
     <Box />
     <CameronSlogan message="Hello From the Index Pages" />
     <CameronSlogan />
