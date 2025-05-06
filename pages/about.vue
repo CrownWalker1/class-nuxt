13f72da12f73ef990522b222fc69e88b9c663a35
CrownWalker1 <cameron.robert.benson@gmail.com>
2025-05-06 08:58:44 -0600

    1

--- pages/about.vue
+++ pages/about.vue
@@ -3,7 +3,7 @@
     <PageHeader title="About Page" />
     </div>
 <div>
-    <img class="h-100" src="file:///C:/Users/Camer/Desktop/Sites/Dev%20Class%202025/Webdev-2025/class-nuxt/public/IMG_2592.jpg" alt="">
+    <img class="h-100" src="C:\Users\Camer\Desktop\Sites\Dev Class 2025\Webdev-2025\class-nuxt\public\Self Photo.jpg" alt="">
 </div>
 
     <div class="space-y-4">
